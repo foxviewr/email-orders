8e06d6253b4ce47c5bc4a8a34f4d07f74f209a06
Pedro Teles <pedroteles_19@hotmail.com>
2024-08-09 10:38:36 +0200

    makefile code structure updated

--- makelib/app-info.c
+++ makelib/app-info.c
@@ -1,10 +1,10 @@
 define BackendPackageJson
-$(shell node -p "require('./backend/composer.json').$(1)")
+    $(shell node -p "require('./backend/composer.json').$(1)")
 endef
 BACKEND_VERSION := $(call BackendPackageJson,version)
 
 define FrontendPackageJson
-$(shell node -p "require('./frontend/package.json').$(1)")
+    $(shell node -p "require('./frontend/package.json').$(1)")
 endef
 FRONTEND_VERSION := $(call FrontendPackageJson,version)
 AUTHOR := $(call FrontendPackageJson,author)
